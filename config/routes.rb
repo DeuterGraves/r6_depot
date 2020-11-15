9ae3ae650baed2fcbd81f4f916ebe0b6c755bc11
Caroline Graves <caroline.graves@freeagent.com>
2020-11-15 17:22:44 +0000

    WIP: set the app root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'store/index'
+  # as 'store_index' means we can use 'store_index_path' and 'store_index_url'
+  root 'store#index', as: 'store_index'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

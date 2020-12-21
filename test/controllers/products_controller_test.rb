45c8349c6288edff5797b1d2427aad86de615a3b
Caroline Graves <caroline.graves@freeagent.com>
2020-12-21 21:44:27 +0000

    prep fixtures for new tests coming

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @product = products(:one)
+    @product = products(:apps)
     @title = "Thee Greate Book #{rand(1000)}"
   end
 
@@ -57,7 +57,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "can't delete product in cart" do
     assert_difference("Product.count", 0) do
-      delete product_url(products(:two))
+      delete product_url(products(:testing))
     end
 
     assert_redirected_to products_url
